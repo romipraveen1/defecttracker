6925b07587df8af75d3f3d764ef5e2b10d3c1c61
Thanushan19 <thanu19@live.com>
2019-06-13 10:49:16 +0530

    Thanushan

---
+++ src/main/java/com/sgic/defecttrackers/entity/DBFile.java
@@ -0,0 +1,70 @@
+package com.sgic.defecttrackers.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "files")
+public class DBFile {
+    
+//    @GeneratedValue(generator = "uuid")
+//    @GenericGenerator(name = "uuid", strategy = "uuid2")
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id;
+
+    public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	private String fileName;
+
+    private String fileType;
+
+    @Lob
+    private byte[] data;
+
+    public DBFile() {
+
+    }
+
+    public DBFile(String fileName, String fileType, byte[] data) {
+        this.fileName = fileName;
+        this.fileType = fileType;
+        this.data = data;
+    }
+
+   
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+}
